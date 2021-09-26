6616f06b3244b30dda8ff8a9109868c147cca928
Magda <magogono@gmail.com>
2021-09-26 17:57:57 +0200

    Poprawka surrogate_ECG_signal !!! - teraz sumuje w pionie. + Dodanie testu.

--- magda/main_file.py
+++ magda/main_file.py
@@ -11,36 +11,155 @@ Computing horizontal line integrals (sum over x).
 The vertical motion between two successive frames is estimated by identifying the shift along the
 vertical axis that minimizes the sum of squared differences between the corresponding vectors (horizontal line integrals).
 """
-def surrogate_ECG_horline_integrals(images):
-    hor_integrals = np.sum(images, axis=1)
+def surrogate_ECG_horline_integrals(images, max_shift=-1):
+    type = images.dtype
+    hor_integrals = np.sum(images, axis=2)
     num_of_img = hor_integrals.shape[0]
 
     height = hor_integrals.shape[1]
+    max_shift = height - 1 if max_shift is -1 else max_shift
 
     best_shifts = np.empty([0])
 
     for ind in range(num_of_img - 1):
         vec_1 = hor_integrals[ind]
-        vec_2 = np.concatenate((np.zeros(height, dtype=TYPE),
+        vec_2 = np.concatenate((np.zeros(max_shift, dtype=type),
                                 hor_integrals[ind + 1],
-                                np.zeros(height, dtype=TYPE)))
+                                np.zeros(max_shift, dtype=type)))
         sum_results = np.empty([0])
 
         """ pierwszy obraz jest stały, a drugi "przesuwamy" względem niego """
-        for sh in range(-height + 1, height):
-            window = vec_2[height + sh: 2 * height + sh]
+        for sh in range(-max_shift, max_shift+1):
+            window = vec_2[max_shift + sh: height + max_shift + sh]
             s = np.sum((vec_1 - window) ** 2)
             sum_results = np.append(sum_results, s)
 
 #        print(f"IMG {ind} - Array of sum for every shift applied")
 #        print(sum_results)
 
-        best_sh = np.argmin(sum_results) - height + 1
+        best_sh = np.argmin(sum_results) - max_shift
         best_shifts = np.append(best_shifts, best_sh)
     return best_shifts
 
 
-if __name__ == '__main__':
+def plot_ECG_signal(heights, title=''):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    # ax.scatter(range(len(images)), images)
+    ax.plot(range(len(heights)), heights, marker='o', label='normal vals')
+    ax.plot(range(len(heights)), abs(heights), marker='o', color='red', label='abs')
+    ax.plot(range(len(heights)), np.zeros([heights.shape[0]]))
+    ax.set_title(title)
+    ax.legend(frameon=False)
+
+
+def test_algorithm():
+    print("============= test =============")
+    test1 = np.array([
+        [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        [[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]],
+        ])
+
+    display_multiple_img(test1, rows=11)
+
+    test_sh = surrogate_ECG_horline_integrals(test1, max_shift=3)
+    test_sh = abs(test_sh)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.plot(range(len(test_sh)), test_sh,  marker='o')
+    plt.show()
+
+    print("============= end test =============")
+
+
+def show_results_from_dicom():
     print("Type DICOM file path:\n")
     file_path = "numer2\\exam.dcm"
     ds = dcmread(file_path)
@@ -54,6 +173,7 @@ if __name__ == '__main__':
 
     for ind in range(images.shape[0]):
         img = ( 255 * frangi(images_f[ind]) ).astype(TYPE)         # po prostu ucina końcówki, bez zaokrąglania
+#        img = frangi(images_f[ind])
         img = np.reshape(img, (1, 512, 512))
         images_frangi = np.append(images_frangi, img, axis=0)
 
@@ -67,27 +187,27 @@ if __name__ == '__main__':
     # bin_imgs = cv2.threshold(images_frangi, 10.0, 255.0, cv2.THRESH_BINARY_INV)[1]
     # display_multiple_img(bin_imgs, 4, 'gray', title='binary inv 10')
 
-    plt.show()
+    #    plt.show()
 
     print("============== algorithm ==================")
 
-    best_shifts_1 = surrogate_ECG_horline_integrals(images_frangi)
-    best_shifts_2 = surrogate_ECG_horline_integrals(images)
+    m_sh = 512
+    best_shifts_1 = surrogate_ECG_horline_integrals(images_frangi, max_shift=m_sh)
+    best_shifts_2 = surrogate_ECG_horline_integrals(images, max_shift=m_sh)
 
     """ 
         Plotting best shifts between each two adjacent images (0:1, 1:2 ... N-1:N)
     """
     print("============== Surrogate ECG ==================")
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(range(len(best_shifts_1)), best_shifts_1)
-    ax.set_title("Surrogate ECG signal - frangi")
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.plot(range(len(best_shifts_2)), best_shifts_2)
-    ax.set_title("Surrogate ECG signal - original images")
+    plot_ECG_signal(best_shifts_1, f"Surrogate ECG signal - frangi, max_sh = {m_sh}")
+    plot_ECG_signal(best_shifts_2, f"Surrogate ECG signal - original images, max_sh = {m_sh}")
 
     plt.show()
+
+
+if __name__ == '__main__':
+     # show_results_from_dicom()
+     test_algorithm()
 
    # Frame 'Relative Time' (n) = Frame Delay + Frame Time * (n-1)
